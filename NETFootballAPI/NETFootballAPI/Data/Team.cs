704121c2dfcb7d09055645a08cb0cd5f6f6f525e
Morten Pedersen <moped47@gmail.com>
2020-08-19 16:07:44 +0200

    Added team DTO

---
+++ NETFootballAPI/NETFootballAPI/Data/Team.cs
@@ -0,0 +1,28 @@
+#nullable enable
+using Newtonsoft.Json;
+
+namespace NETFootballAPI
+{
+    public class Team
+    {
+        [JsonProperty("team_id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Code { get; set; }
+        public string Logo { get; set; }
+        [JsonProperty("is_national")]
+        public bool IsNational { get; set; }
+        public string Country { get; set; }
+        public int Founded { get; set; }
+        [JsonProperty("venue_name")]
+        public string VenueName { get; set; }
+        [JsonProperty("venue_surface")] 
+        public string VenueSurface { get; set; }
+        [JsonProperty("venue_address")] 
+        public string VenueAddress { get; set; }
+        [JsonProperty("venue_city")]
+        public string VenueCity { get; set; }
+        [JsonProperty("venue_capacity")]
+        public int VenueCapacity { get; set; }
+    }
+}
