a2c929975cf93b17d01d581edd7faec9809e6200
Morten Pedersen <moped47@gmail.com>
2020-08-22 15:22:57 +0200

    Added interface for Fixtures endpoint

---
+++ NETFootballAPI/NETFootballAPI/Interface/IFixtureHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NETFootballAPI
+{
+    public interface IFixtureHandler : IBaseApi
+    {
+        Task<Fixture> GetFixtureByAsync(int fixtureId, string timeZone = null);
+        Task<List<Fixture>> GetAllLiveFixturesAsync();
+        Task<List<Fixture>> GetAllLiveFixturesBySeveralLeaguesAsync(List<int> leagueId, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByDateAsync(DateTime date, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByLeagueAsync(int leagueId, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByLeagueAndDateAsync(int leagueId, DateTime date, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByLeagueAndRoundAsync(int leagueId, string round, string timeZone = null);
+        Task<List<Fixture>> GetNextNumberOfFixturesByLeagueAsync(int leagueId, int numberOfFixtures, string timeZone = null);
+        Task<List<Fixture>> GetLastNumberOfFixturesByLeagueAsync(int leagueId, int numberOfFixtures, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByTeamAsync(int teamId, string timeZone = null);
+        Task<List<Fixture>> GetAllFixturesByTeamAndLeagueAsync(int teamId, int leagueId, string timeZone = null);
+        Task<List<Fixture>>
+            GetNextNumberOfFixturesByTeamAsync(int teamId, int numberOfFixtures, string timeZone = null);
+        Task<List<Fixture>>
+            GetLastNumberOfFixturesByTeamAsync(int teamId, int numberOfFixtures, string timeZone = null);
+    }
+}
