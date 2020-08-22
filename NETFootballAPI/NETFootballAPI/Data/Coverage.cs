81f3abd3d55ab5a62c1b8b3e25f239a6fa95ec6b
Morten Pedersen <moped47@gmail.com>
2020-08-22 15:02:22 +0200

    Added and refactored DTOs to more specific names

--- NETFootballAPI/NETFootballAPI/Data/Coverage.cs
+++ NETFootballAPI/NETFootballAPI/Data/Coverage.cs
@@ -1,9 +1,11 @@
+using Newtonsoft.Json;
+
 namespace NETFootballAPI
 {
     public class Coverage
     {
         public bool Standings { get; set; }
-        public Fixture Fixtures { get; set; }
+        public FixtureCoverage Fixtures { get; set; }
         public bool Players { get; set; }
         public bool TopScorers { get; set; }
         public bool Predictions { get; set; }
---
+++ NETFootballAPI/NETFootballAPI/Data/Score.cs
@@ -0,0 +1,10 @@
+namespace NETFootballAPI
+{
+    public class Score
+    {
+        public string HalfTime { get; set; }
+        public string FullTime { get; set; }
+        public string ExtraTime { get; set; }
+        public string Penalty { get; set; }
+    }
+}
