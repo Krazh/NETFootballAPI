e889d45d32f10afcc4c056dc794f19f34cac578b
Morten Pedersen <moped47@gmail.com>
2021-02-18 19:52:04 +0100

    Updated Team data model to handle new API

--- NETFootballAPI/NETFootballAPI/Data/Team.cs
+++ NETFootballAPI/NETFootballAPI/Data/Team.cs
@@ -14,16 +14,7 @@ namespace NETFootballAPI
         public bool IsNational { get; set; }
         public string Country { get; set; }
         public int Founded { get; set; }
-        [JsonProperty("venue_name")]
-        public string VenueName { get; set; }
-        [JsonProperty("venue_surface")] 
-        public string VenueSurface { get; set; }
-        [JsonProperty("venue_address")] 
-        public string VenueAddress { get; set; }
-        [JsonProperty("venue_city")]
-        public string VenueCity { get; set; }
-        [JsonProperty("venue_capacity")]
-        public int VenueCapacity { get; set; }
+        public Venue Venue { get; set; }
         [JsonProperty("statistics")]
         public Matches MatchStatistics { get; set; }
     }
